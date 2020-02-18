241d708a0918ebde057f5dbba7ae93d3e129d2fc
Jacobo Tabernero <jacoborus@gmail.com>
2020-02-18 14:44:18 +0100

    Fix: Tests require wrong tests file

--- src/tests.ts
+++ src/tests.ts
@@ -2,8 +2,7 @@ const arr = [
   require('./boxes-test.ts'),
   require('./methods-test.ts'),
   require('./modifiers-test.ts'),
-  require('./emitter-test.ts'),
-  require('./weak-emitter-test.ts')
+  require('./emitter-test.ts')
 ]
 
 console.log('test files:', arr.length)

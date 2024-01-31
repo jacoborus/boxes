6b71ed23a38bcc877a598660c2104e057c0a0560
Jacobo Tabernero Rey <jacoborus@gmail.com>
2024-01-31 21:52:23 +0100

    Add actions. Missing boxes mutations

--- store.ts
+++ store.ts
@@ -1,6 +1,11 @@
 import type { Dict, ReadonlyBasic } from "./boxes.ts";
 import { createBox } from "./boxes.ts";
 
+type GettersConfig<S extends Dict> = Record<
+  string,
+  (state: ReadonlyBasic<S>, getters: Getters<S, GettersConfig<S>>) => unknown
+>;
+
 type GetterFn<
   S extends Dict,
   G extends GettersConfig<S>,
@@ -11,36 +16,69 @@ type Getters<S extends Dict, G extends GettersConfig<S>> = {
   [K in keyof G]: GetterFn<S, G, K>;
 };
 
-type GettersConfig<S extends Dict> = Record<
+type ActionsConfig<S extends Dict, G extends GettersConfig<S>> = Record<
   string,
-  (state: ReadonlyBasic<S>, getters: Getters<S, GettersConfig<S>>) => unknown
+  (
+    state: ReadonlyBasic<S>,
+    getters: Getters<S, G>,
+    actions: Actions<S, G, ActionsConfig<S, G>>,
+  ) => unknown
 >;
 
-export function createStore<S extends Dict, G extends GettersConfig<S>>(
-  config = { state: () => {}, getters: {} } as { state: () => S; getters: G },
+type Actions<
+  S extends Dict,
+  G extends GettersConfig<S>,
+  A extends ActionsConfig<S, G>,
+> = {
+  [K in keyof A]: ActionFn<S, G, A, K>;
+};
+
+type ActionFn<
+  S extends Dict,
+  G extends GettersConfig<S>,
+  A extends ActionsConfig<S, G>,
+  K extends keyof A,
+> = () => ReturnType<A[K]>;
+
+export function createStore<
+  S extends Dict,
+  G extends GettersConfig<S>,
+  A extends ActionsConfig<S, G>,
+>(
+  initState = (() => {}) as () => S,
+  gettersConfig: G | null,
+  actionsConfig?: A,
 ) {
-  const box = createBox(config.state());
+  gettersConfig = gettersConfig || {} as G;
+  actionsConfig = actionsConfig || {} as A;
+
+  const box = createBox(initState());
   const state = box();
 
   const getters = {} as Getters<S, G>;
 
-  for (const i in config.getters) {
-    const getterFactory = config.getters[i];
+  for (const i in gettersConfig) {
+    const getterFactory = gettersConfig[i];
     getters[i] = function () {
       return getterFactory(state, getters);
     } as GetterFn<S, G, typeof i>;
   }
 
+  const actions = {} as Actions<S, G, A>;
+
+  for (const i in actionsConfig) {
+    const actionFactory = actionsConfig[i];
+    actions[i] = function () {
+      return actionFactory(state, getters, actions);
+    } as ActionFn<S, G, A, typeof i>;
+  }
+
   return {
     state,
     $reset() {
-      box.update(box(), config.state());
+      box.update(box(), initState());
     },
     getters,
+    actions,
   };
 }
-
-// type StoreConfig<S extends Dict, G extends GettersConfig<S>> = {
-//   state: () => S;
-//   getters: G;
-// };
--- test/store_test.ts
+++ test/store_test.ts
@@ -3,11 +3,11 @@ import { createStore } from "../store.ts";
 // import { watch } from "../boxes.ts";
 
 Deno.test({
-  name: "basic store",
+  name: "Store getters",
   fn() {
-    const { state, getters } = createStore({
-      state: () => ({ hi: "hola", num: 2 }),
-      getters: {
+    const { state, getters } = createStore(
+      () => ({ hi: "hola", num: 2 }),
+      {
         sayHi(state) {
           return state.hi + " Mundo";
         },
@@ -15,7 +15,7 @@ Deno.test({
           return getters.sayHi() + "!" + state.num;
         },
       },
-    });
+    );
 
     // state.hi === 5;
     // state.asdfasdf === 5;
@@ -24,5 +24,34 @@ Deno.test({
     assertEquals(state.hi, "hola");
     assertEquals(getters.sayHi(), "hola Mundo");
     assertEquals(getters.greetings(), "hola Mundo!2");
+  },
+});
+
+Deno.test({
+  name: "Store actions",
+  fn() {
+    const { state, getters, actions } = createStore(
+      (): {
+        name: string;
+        age?: number;
+        member?: boolean;
+      } => ({ name: "boxes" }),
+      {
+        sayHi(state) {
+          return "Hi " + state.name;
+        },
+        greetings(state, getters) {
+          return getters.sayHi() +
+            `!, you are ${state.member ? "not" : ""} a member`;
+        },
+      },
+      {
+        changeName(box) {},
+      },
+    );
+
+    assertEquals(state.name, "boxes");
+    assertEquals(getters.sayHi(), "Hi boxes");
+    assertEquals(getters.greetings(), "Hi boxes! you are not a member");
   },
 });

204af327e3e1f1a46fbaff372f39dc1c4eb7e384
Jacobo Tabernero <jacoborus@gmail.com>
2024-02-10 21:01:14 +0100

    Add more tests to update

--- test/update_test.ts
+++ test/update_test.ts
@@ -3,7 +3,7 @@ import { createBox } from "../boxes.ts";
 import { watchProp } from "../src/reactive.ts";
 
 Deno.test({
-  name: "basic update",
+  name: "Update simple object",
   fn() {
     const obj = { a: 1 };
     const box = createBox(obj);
@@ -18,5 +18,55 @@ Deno.test({
     off();
     box.update(data, { a: 6 });
     assertEquals(control, 4);
+  },
+});
+
+Deno.test({
+  name: "Update object",
+  fn() {
+    const obj = { a: 1, b: "abc", o: { x: "x", y: "y" } };
+    const box = createBox<{
+      a: number;
+      b?: string;
+      o: { x: string; y: string };
+    }>(obj);
+    const data = box();
+    let control = 0;
+    const off = watchProp(data, "a", (value) => {
+      control = value;
+    });
+    box.update(data, { a: 4, o: { x: "z", y: "z" } });
+    assertEquals(data.a, 4, "update works");
+    assertEquals(data.b, undefined, "update works");
+    assertEquals(data.o.x, "z", "update works");
+    assertEquals(control, 4, "watch works");
+    off();
+    box.update(data, { a: 6, o: { x: "hola", y: "z" } });
+    assertEquals(control, 4);
+  },
+});
+
+Deno.test({
+  name: "Update deep",
+  fn() {
+    const obj = { a: 1, b: "abc", o: { x: "x", y: "y" } };
+    const box = createBox<{
+      a: number;
+      b?: string;
+      o: { x: string; y: string };
+    }>(obj);
+    const data = box();
+    let control = "";
+    const off = watchProp(data.o, "x", (value) => {
+      control = value;
+    });
+    box.update(data.o, { x: "z", y: "z" });
+    assertEquals(data.a, 1, "update works");
+    assertEquals(data.b, "abc", "update works");
+    assertEquals(data.o.x, "z", "update works");
+    assertEquals(control, "z", "watch works");
+    off();
+    box.update(data, { a: 6, o: { x: "hola", y: "z" } });
+    assertEquals(control, "z");
   },
 });

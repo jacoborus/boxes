3c141ff7c0f1ee311a570362784b09ab1ca74351
Jacobo Tabernero <jacoborus@gmail.com>
2024-02-19 04:38:22 +0100

    Test batch

---
+++ test/batch_test.ts
@@ -0,0 +1,25 @@
+import { assertEquals } from "assert";
+import { batch, createBox, watchProp } from "../boxes.ts";
+
+Deno.test({
+  name: "batch",
+  fn() {
+    const box = createBox({ a: 1 });
+    const data = box();
+    let count = 0;
+    const off = watchProp(data, "a", () => {
+      count++;
+    });
+    batch(() => {
+      box.update(data, { a: 4 });
+      box.update(data, { a: 6 });
+      box.patch(data, { a: 9 });
+    });
+    assertEquals(count, 1, "batch works");
+    box.update(data, { a: 88 });
+    assertEquals(count, 2);
+    off();
+    box.update(data, { a: 6 });
+    assertEquals(count, 2);
+  },
+});
